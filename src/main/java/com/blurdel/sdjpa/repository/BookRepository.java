b4c12964e26fc6ae112844cf7945591fb8ecc8dc
blurdel <5648737+blurdel@users.noreply.github.com>
2023-02-20 18:58:51 -0500

    Implemented getting a Stream of Book and using forEach on it

--- src/main/java/com/blurdel/sdjpa/repository/BookRepository.java
+++ src/main/java/com/blurdel/sdjpa/repository/BookRepository.java
@@ -1,6 +1,7 @@
 package com.blurdel.sdjpa.repository;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.Nullable;
@@ -15,5 +16,7 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 	
 	@Nullable
 	Book getByTitle(@Nullable String title);
+	
+	Stream<Book> findAllByTitleNotNull();
 	
 }
--- src/test/java/com/blurdel/sdjpa/BookRepositoryTest.java
+++ src/test/java/com/blurdel/sdjpa/BookRepositoryTest.java
@@ -1,9 +1,10 @@
 package com.blurdel.sdjpa;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,8 +26,22 @@ public class BookRepositoryTest {
 
 	@Autowired
 	BookRepository bookRepo;
+
 	
+	@Test
+	void testBookStream() {
+		AtomicInteger count = new AtomicInteger();
+		
+		bookRepo.findAllByTitleNotNull().forEach(book -> {
+			count.incrementAndGet();
+		});
+		
+		assertThat(count.get()).isGreaterThan(4);
+	}
 	
+	// NOTE: these test(s) required adding package-info.java to repositories to include: @org.springframework.lang.NonNullApi
+	// Tells Spring to implement the null behavior for any repository in the package
+	// Otherwise a unit test fails
 	@Test
 	void testEmptyResultException() {
 		
